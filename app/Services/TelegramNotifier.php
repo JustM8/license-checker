7d153618cf96e22f2afcbbf186b80f641bd39b38
Mate <gav.sqrt@gmail.com>
2026-02-28 15:35:36 +0200

    Tg to env

--- app/Services/TelegramNotifier.php
+++ app/Services/TelegramNotifier.php
@@ -2,26 +2,31 @@
 
 namespace App\Services;
 
+use Illuminate\Support\Facades\Http;
+use Illuminate\Support\Facades\Log;
+
 class TelegramNotifier
 {
     public static function send(string $message): void
     {
-        $chatId = '386327020'; // <- твій chat_id
-        $token = '8141281031:AAFYNfq2BhHgYM6tcywIykSuTIC3JxYQLxE'; // <- заміни на правильний
-
-        $url = "https://api.telegram.org/bot{$token}/sendMessage";
-
-        $payload = [
-            'chat_id' => $chatId,
-            'text' => $message,
-            'parse_mode' => 'Markdown',
-        ];
+        $token = config('services.telegram.bot_token');
+        $chatId = config('services.telegram.chat_id');
 
         try {
-            file_get_contents($url . '?' . http_build_query($payload));
+
+            Http::post(
+                "https://api.telegram.org/bot{$token}/sendMessage",
+                [
+                    'chat_id' => $chatId,
+                    'text' => $message,
+                    'parse_mode' => 'Markdown',
+                ]
+            );
+
         } catch (\Exception $e) {
-            \Log::error('Помилка надсилання повідомлення в Telegram: ' . $e->getMessage());
+
+            Log::error($e->getMessage());
+
         }
     }
 }
-
